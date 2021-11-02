82413c2e6542ec4666884182693d11bb75969d60
Jackson Mills <jmills@ripple.com>
2021-11-02 12:46:23 -0700

    Add ability to go left or right in tabs

--- dashboard/main.py
+++ dashboard/main.py
@@ -8,12 +8,14 @@ def init_colors():
     for i in range(NUM_COLORS):
         curses.init_pair(i+1, i, curses.COLOR_WHITE)
 
-def print_tabs(stdscr, tab_list: List[str]) -> None:
+def print_tabs(stdscr, tab_list: List[str], curr_tab) -> None:
     column = 2
     row = 10
 
     for i in range(len(tab_list)):
         tab = tab_list[i]
+        if(tab == curr_tab):
+            tab = '**' + tab + '**'
         stdscr.addstr(column, row, tab, curses.color_pair((i+1) % NUM_COLORS))
         row += len(tab) + 5
 
@@ -24,10 +26,40 @@ def main(stdscr):
     # Clear screen
     stdscr.clear()
     
-    print_tabs(stdscr, ["setup", "peers", "consensus"])
-
+    tabs = ["home", "setup", "peers", "consensus"]
+    curr_tab = tabs[0]
+    print_tabs(stdscr, tabs, curr_tab)
     stdscr.refresh()
-    stdscr.getkey()
+
+    stdscr.keypad(True)
+    c = stdscr.getkey()
+    while(c != 'q'):
+        if(c == 'KEY_LEFT'):
+            stdscr.clear()
+            curr_tab = tabs[tabs.index(curr_tab) - 1]
+            print_tabs(stdscr, tabs, curr_tab)
+            stdscr.refresh()
+
+        if(c == 'KEY_RIGHT'):
+            stdscr.clear()
+            index = tabs.index(curr_tab)
+            if(index + 1 >= len(tabs)):
+                curr_tab = tabs[0]
+            else:
+                curr_tab = tabs[index + 1]
+            print_tabs(stdscr, tabs, curr_tab)
+            stdscr.refresh()
+
+
+        if(c == 'q'):
+            break
+
+        stdscr.refresh()
+
+        c = stdscr.getkey()
+
 
 if __name__ == "__main__":
     curses.wrapper(main)
+
+
