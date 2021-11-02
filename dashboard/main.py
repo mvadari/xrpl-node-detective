45797893cc7e1eb08af25579238e707bbdfeb8f3
Mayukha Vadari <mvadari@ripple.com>
2021-11-02 16:07:48 -0400

    switch to object oriented

--- dashboard/main.py
+++ dashboard/main.py
@@ -6,61 +6,71 @@ NUM_COLORS = 7 # curses has 8, but white is one of them
 
 TABS = ["home", "setup", "peers", "consensus"]
 
+curr_tab = TABS[0]
+
 def init_colors():
     for i in range(NUM_COLORS):
         curses.init_pair(i+1, i, curses.COLOR_WHITE)
 
-def print_tabs(stdscr, curr_tab) -> None:
-    column = 2
-    row = 10
+class Interface:
+    def __init__(self) -> None:
+        pass
 
-    for i in range(len(TABS)):
-        tab = TABS[i]
-        if(tab == curr_tab):
-            tab = '**' + tab + '**'
-        stdscr.addstr(column, row, tab, curses.color_pair((i+1) % NUM_COLORS))
-        row += len(tab) + 5
+    def run(self, stdscr):
+        self.stdscr = stdscr
+        curses.noecho()
+        init_colors()
+        self.stdscr.keypad(True)
+        # Clear screen
+        self.stdscr.clear()
+        
+        self.print_tabs(curr_tab)
+        self.stdscr.refresh()
 
-def main(stdscr):
-    curses.noecho()
-    init_colors()
-    stdscr.keypad(True)
-    # Clear screen
-    stdscr.clear()
+        self.stdscr.keypad(True)
+        c = self.stdscr.getkey()
+        while(c != 'q'):
+            if(c == 'q'):
+                break
+            
+            self.handle_key(c)
+
+            self.stdscr.refresh()
+
+            c = self.stdscr.getkey()
     
-    curr_tab = TABS[0]
-    print_tabs(stdscr, curr_tab)
-    stdscr.refresh()
+    def print_tabs(self, curr_tab) -> None:
+        column = 2
+        row = 10
 
-    stdscr.keypad(True)
-    c = stdscr.getkey()
-    while(c != 'q'):
+        for i in range(len(TABS)):
+            tab = TABS[i]
+            if(tab == curr_tab):
+                tab = '**' + tab + '**'
+            self.stdscr.addstr(column, row, tab, curses.color_pair((i+1) % NUM_COLORS))
+            row += len(tab) + 5
+    
+    def handle_key(self, c: str) -> None:
+        global curr_tab
         if(c == 'KEY_LEFT'):
-            stdscr.clear()
+            self.stdscr.clear()
             curr_tab = TABS[TABS.index(curr_tab) - 1]
-            print_tabs(stdscr, curr_tab)
-            stdscr.refresh()
+            self.print_tabs(curr_tab)
+            self.stdscr.refresh()
 
         if(c == 'KEY_RIGHT'):
-            stdscr.clear()
+            self.stdscr.clear()
             index = TABS.index(curr_tab)
             if(index + 1 >= len(TABS)):
                 curr_tab = TABS[0]
             else:
                 curr_tab = TABS[index + 1]
-            print_tabs(stdscr, curr_tab)
-            stdscr.refresh()
-
-
-        if(c == 'q'):
-            break
-
-        stdscr.refresh()
-
-        c = stdscr.getkey()
+            self.print_tabs(curr_tab)
+            self.stdscr.refresh()
 
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    interface = Interface()
+    curses.wrapper(interface.run)
 
 
