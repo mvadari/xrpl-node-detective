f0eef4f566b506f5f78e2baa2f213b7dc1c11ca0
Mayukha Vadari <mvadari@ripple.com>
2021-11-02 14:59:07 -0400

    get tabs in colors

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,16 +2,24 @@ from typing import List
 
 import curses
 
+NUM_COLORS = 7 # curses has 8, but white is one of them
+
+def init_colors():
+    for i in range(NUM_COLORS):
+        curses.init_pair(i+1, i, curses.COLOR_WHITE)
+
 def print_tabs(stdscr, tab_list: List[str]) -> None:
     column = 2
     row = 10
 
-    for tab in tab_list:
-        stdscr.addstr(column, row, tab, curses.A_REVERSE)
+    for i in range(len(tab_list)):
+        tab = tab_list[i]
+        stdscr.addstr(column, row, tab, curses.color_pair((i+1) % NUM_COLORS))
         row += len(tab) + 5
 
 def main(stdscr):
     curses.noecho()
+    init_colors()
     stdscr.keypad(True)
     # Clear screen
     stdscr.clear()
